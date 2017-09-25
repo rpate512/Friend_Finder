e2e655103c10d3b984482d07f7fac705c9833858
Ryan Pate <ryanpate@Ryans-MacBook-Pro.local>
2017-09-25 10:40:43 -0500

    Final

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+  {
+    name: "Johnny Depp",
+    photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTM0ODU5Nzk2OV5BMl5BanBnXkFtZTcwMzI2ODgyNQ@@._V1_UY1200_CR106,0,630,1200_AL_.jpg",
+    scores: [
+      "2",
+      "1",
+      "5",
+      "3",
+      "2",
+      "2",
+      "2",
+      "3",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Arnold Schwarzenegger",
+    photo: "http://www.uh.edu/news-events/stories/2017/March/Arnold%20Schwarzenegger.jpeg",
+    scores: [
+      "2",
+      "3",
+      "1",
+      "1",
+      "2",
+      "5",
+      "5",
+      "5",
+      "5",
+      "1"
+    ]
+  },
+  {
+    name: "Emma Stone",
+    photo: "https://i.pinimg.com/736x/9d/d3/76/9dd3768d6b655275fe9d4822179b3f65--emily-stone-enma-stone.jpg",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "1",
+      "5",
+      "5",
+      "5",
+      "1"
+    ]
+  },
+  {
+    name: "Chris Hemsworth",
+    photo: "https://pixel.nymag.com/imgs/fashion/daily/2017/07/25/Chris-Hemsworth/25-Chris-Hemsworth.jpg",
+    scores: [
+      "2",
+      "4",
+      "1",
+      "2",
+      "3",
+      "4",
+      "4",
+      "1",
+      "2",
+      "5"
+    ]
+  },
+  {
+    name: "Mila Kunis",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/20120712_Mila_Kunis_%40_Comic-con_cropped.jpg/1200px-20120712_Mila_Kunis_%40_Comic-con_cropped.jpg",
+    scores: [
+      "5",
+      "1",
+      "2",
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "1",
+      "5"
+    ]
+  },
+  {
+    name: "Joe Biden",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Official_portrait_of_Vice_President_Joe_Biden.jpg/1200px-Official_portrait_of_Vice_President_Joe_Biden.jpg",
+    scores: [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  }
+
+];
+
+module.exports = friends;
